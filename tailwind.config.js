d80760bf30d86f99b1c7f7d503804fbf5560e9ec
freitzzz <1160907@isep.ipp.pt>
2023-06-14 20:46:25 +0100

    feat: install ripple ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('rippleui')]
+};
