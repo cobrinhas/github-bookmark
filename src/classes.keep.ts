62acf050a031e9c1146c9a13a0b6c52494adf7da
freitzzz <1160907@isep.ipp.pt>
2023-06-17 18:42:36 +0100

    feat: show spinner while issue request is triggered and status message after request finishes

---
+++ src/classes.keep.ts
@@ -0,0 +1,10 @@
+/**
+ * Classes Tailwind does not know at compile time, but that I want to use at runtime (*warning*: hacky)
+ */
+const classes = [
+    'text-success',
+    'text-error',
+    'text-warning',
+];
+
+classes.length = 0;
--- src/dom/form.ts
+++ src/dom/form.ts
@@ -3,6 +3,8 @@ const githubApiTokenStorageKey = 'github-bookmark.api-token';
 
 const memoryStorage = <Record<string, string>>{};
 
+type StatusType = 'info' | 'error' | 'warning' | 'success';
+
 export function load() {
 	const websiteUrlInput = document.getElementById('website-url');
 	const websiteTitleInput = document.getElementById('website-title');
@@ -57,6 +59,8 @@ export function load() {
 }
 
 export function save() {
+	showSpinner();
+
 	const websiteUrlInput = document.getElementById('website-url');
 	const websiteTitleInput = document.getElementById('website-title');
 	const githubRepoUrlInput = document.getElementById('github-repo-url');
@@ -94,24 +98,48 @@ export function save() {
 	}
 
 	if (!githubRepoUrl) {
+		setStatusMessage(
+			'To proceed, please insert the GitHub repo url to create issues.'
+		);
+
 		return;
 	}
 
 	if (!githubApiToken) {
+		setStatusMessage(
+			'To proceed, please insert an API token with permissions to create issues on target repo.'
+		);
+
 		return;
 	}
 
 	if (!websiteUrlInput || !(websiteUrlInput instanceof HTMLInputElement)) {
+		setStatusMessage(
+			`oops, looks like Website URL is misssing? ${websiteUrlInput}`
+		);
+
 		return;
 	}
 
 	if (!websiteTitleInput || !(websiteTitleInput instanceof HTMLInputElement)) {
+		setStatusMessage(
+			`oops, looks like Website Title is misssing? ${websiteTitleInput}`
+		);
+
 		return;
 	}
 
 	const repoUrlRegexp = new RegExp(/.*github.com\/(.+)\/(.+).*/g).exec(
 		githubRepoUrl
 	);
+
+	if (!repoUrlRegexp) {
+		setStatusMessage(
+			`Make sure the repo url follows this syntax: https://github.com/<owner>/<repo>`
+		);
+
+		return;
+	}
 
 	if (repoUrlRegexp) {
 		const owner = repoUrlRegexp[1];
@@ -129,9 +157,35 @@ export function save() {
 				'X-GitHub-Api-Version': '2022-11-28',
 				Authorization: `Bearer ${githubApiToken}`
 			}
-		}).then((result) => {
-			console.log(result);
-		});
+		})
+			.then((result) => {
+				if (result.ok) {
+					result.json().then((json) => setIssueUrl(json.html_url))
+				} else {
+					const statusType: StatusType = 'error';
+					let message: string;
+
+					switch (result.status) {
+						case 401:
+							message = `Did you misstyped your API token?`;
+							break;
+						case 403:
+							message = `Does your API token have the "Read and Write access to issues" permission for the selected repo?`;
+							break;
+						case 404:
+							message = `Does this repo really exist?`;
+							break;
+						default:
+							message = `I wasn't expecting this error from GitHub API!`;
+					}
+
+					setStatusMessage(`${message} (${result.status}) `, statusType);
+
+				}
+			})
+			.catch((err) => {
+				setStatusMessage(err, 'error');
+			}).finally(hideSpinner);
 	}
 }
 
@@ -150,4 +204,93 @@ function loadInMemoryStorage() {
 	});
 
 	return get;
+}
+
+function setStatusMessage(message: string | undefined, type: StatusType = 'info') {
+	const formStatusDiv = document.getElementById('form-status');
+	const statusP = document.getElementById('status');
+
+	if (!formStatusDiv) {
+		return;
+	}
+
+	if (!message) {
+		formStatusDiv.classList.add('hidden');
+
+		return;
+	}
+
+	formStatusDiv.classList.remove('hidden');
+
+	if (statusP instanceof HTMLParagraphElement) {
+		statusP.textContent = message;
+
+		const statusTypeCSSClass = `text-${type}`;
+
+		statusP.classList.remove('hidden');
+
+		if (statusP.classList.length > 2) {
+			statusP.classList.remove(statusP.classList.item(2) ?? statusTypeCSSClass);
+		}
+
+		if (type !== 'info') {
+			statusP.classList.add(statusTypeCSSClass);
+		}
+	}
+
+}
+
+function setIssueUrl(url: string) {
+	const formStatusDiv = document.getElementById('form-status');
+	const issueA = document.getElementById('issueUrl');
+
+	if (!formStatusDiv) {
+		return;
+	}
+
+	if (!issueA) {
+		formStatusDiv.classList.add('hidden');
+
+		return;
+	}
+
+	formStatusDiv.classList.remove('hidden');
+
+	if (issueA instanceof HTMLAnchorElement) {
+		issueA.innerText = '🦋 Take me to the issue! 🦋';
+
+		issueA.classList.remove('hidden');
+
+		issueA.setAttribute('href', url);
+	}
+}
+
+function showSpinner() {
+	const formStatusDiv = document.getElementById('form-status');
+	const spinnerDiv = document.getElementById('spinner');
+
+	if (!formStatusDiv) {
+		return;
+	}
+
+	if (formStatusDiv) {
+		formStatusDiv.classList.remove('hidden');
+	}
+
+	if (spinnerDiv) {
+		spinnerDiv.classList.remove('hidden');
+	}
+}
+
+function hideSpinner() {
+	const formStatusDiv = document.getElementById('form-status');
+	const spinnerDiv = document.getElementById('spinner');
+
+	if (!formStatusDiv) {
+		return;
+	}
+
+	if (spinnerDiv) {
+		spinnerDiv.classList.add('hidden');
+	}
 }
